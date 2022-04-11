b61736e18859f244b1fa525788afabe39fb31f33
Clicknetcafe <rama4shared@gmail.com>
2022-04-11 21:02:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Modded by Zerenity.
+   * Contact Me on wa.me/6282337245566
+   * Follow https://github.com/clicknetcafe
 */
 
 require('./config')
